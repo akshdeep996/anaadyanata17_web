fdd87ed444d47dd8ebb6d599284ed08b4b2a1733
rxdsrex <rxdsrex@gmail.com>
2017-02-09 19:24:01 +0530

    Made minor changes and refined the pages

--- js/js_land.js
+++ js/js_land.js
@@ -1,26 +1,14 @@
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2016, Codrops
- * http://www.codrops.com
- */
-;(function(window) {
+;
+(function (window) {
 
 	'use strict';
 
-	// some helper functions
-	/**
-	 * from https://davidwalsh.name/javascript-debounce-function
-	 */
 	function debounce(func, wait, immediate) {
 		var timeout;
-		return function() {
-			var context = this, args = arguments;
-			var later = function() {
+		return function () {
+			var context = this,
+				args = arguments;
+			var later = function () {
 				timeout = null;
 				if (!immediate) func.apply(context, args);
 			};
@@ -33,8 +21,9 @@
 	/**
 	 * from http://stackoverflow.com/a/6700
 	 */
-	Object.size = function(obj) {
-		var size = 0, key;
+	Object.size = function (obj) {
+		var size = 0,
+			key;
 		for (key in obj) {
 			if (obj.hasOwnProperty(key)) size++;
 		}
@@ -45,19 +34,22 @@
 		logo = document.querySelector("#logo"),
 		triggerCtrl = mainContainer.querySelector('.button--trigger'),
 		landingWrapper = mainContainer.querySelector('.landing-wrap'),
-		winsize = {width : window.innerWidth, height : window.innerHeight},
+		winsize = {
+			width: window.innerWidth,
+			height: window.innerHeight
+		},
 		soundCtrl = mainContainer.querySelector('button.button--sound'),
 		isAudioOn = true,
-		
+
 		bubbles = {
-			canvas : null,
-			ctx : null,
-			mousex : winsize.width - 30,
-			mousey : winsize.height - 30,
-			cntr : 0,
-			circleArr : new Array(),
-			requestId : undefined,
-			init : function() {
+			canvas: null,
+			ctx: null,
+			mousex: winsize.width - 30,
+			mousey: winsize.height - 30,
+			cntr: 0,
+			circleArr: new Array(),
+			requestId: undefined,
+			init: function () {
 				this.canvas = document.getElementById('bubbles');
 				this.ctx = this.canvas.getContext('2d');
 				this.canvas.width = winsize.width;
@@ -65,67 +57,71 @@
 
 				// Window resize.
 				var self = this;
-				this.debounceResize = debounce(function() {
-					winsize = {width : window.innerWidth, height : window.innerHeight};
+				this.debounceResize = debounce(function () {
+					winsize = {
+						width: window.innerWidth,
+						height: window.innerHeight
+					};
 					self.canvas.height = winsize.height;
 					self.canvas.width = winsize.width;
 				}, 10);
 				window.addEventListener('resize', this.debounceResize);
 			},
-			loop : function() {
+			loop: function () {
 				this.requestId = requestAnimationFrame(bubbles.loop.bind(this));
 				this.update();
 				this.render();
 			},
-			update : function() {
-				if( this.cntr++ % 1 == 0 ) {
+			update: function () {
+				if (this.cntr++ % 1 == 0) {
 					this.createCircle();
 				}
 
-				for(var circle in this.circleArr) {
+				for (var circle in this.circleArr) {
 					circle = this.circleArr[circle];
-					var max = 2, min = -2;
+					var max = 2,
+						min = -2;
 					circle.x += Math.floor(Math.random() * (max - min + 1)) + min;
-					circle.y -= Math.random()*15;
+					circle.y -= Math.random() * 15;
 				}
 
-				while(this.circleArr.length > 2 && (this.circleArr[0].x + this.circleArr[0].s > winsize.width || this.circleArr[0].x + this.circleArr[0].s < 0 || this.circleArr[0].y + this.circleArr[0].s > winsize.height || this.circleArr[0].y + this.circleArr[0].s < 0) ){
+				while (this.circleArr.length > 2 && (this.circleArr[0].x + this.circleArr[0].s > winsize.width || this.circleArr[0].x + this.circleArr[0].s < 0 || this.circleArr[0].y + this.circleArr[0].s > winsize.height || this.circleArr[0].y + this.circleArr[0].s < 0)) {
 					this.circleArr.shift();
 				}
 			},
-			render : function() {
+			render: function () {
 				// clear
 				this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 				// draw the data
-				for(var circle in this.circleArr) {
+				for (var circle in this.circleArr) {
 					var current = this.circleArr[circle];
 					this.drawCircle(current.x, current.y, current.s);
 				}
 			},
-			createCircle : function() {
-				var tmp = this.circleArr[this.circleArr.length-1];
+			createCircle: function () {
+				var tmp = this.circleArr[this.circleArr.length - 1];
 
 				this.circleArr[this.circleArr.length] = {
 					x: this.mousex,
 					y: this.mousey,
-					s: Math.random()*winsize.height/100
+					s: Math.random() * winsize.height / 100
 				};
 			},
-			drawCircle : function(x, y, radius) {
+			drawCircle: function (x, y, radius) {
 				this.ctx.fillStyle = "rgba(255,255,255,0.5)";
 				this.ctx.beginPath();
-				this.ctx.arc(x,y,radius, 0, Math.PI*2, false);
+				this.ctx.arc(x, y, radius, 0, Math.PI * 2, false);
 				this.ctx.closePath();
 				this.ctx.fill();
 			},
-			start : function() {
-				if( !this.requestId ) {
+			start: function () {
+				if (!this.requestId) {
 					document.onmousemove = this.getMouseCoordinates.bind(this);
 					this.loop();
 				}
 			},
-			stop : function() {
-				if( this.requestId ) {
+			stop: function () {
+				if (this.requestId) {
 					window.cancelAnimationFrame(this.requestId);
 					this.requestId = undefined;
 					document.onmousemove = null;
@@ -133,30 +129,33 @@
 					this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 				}
 			},
-			getMouseCoordinates : function(ev) {
+			getMouseCoordinates: function (ev) {
 				var ev = ev || window.event;
 				this.mousex = ev.pageX;
 				this.mousey = ev.pageY;
 			}
 		},
 		audio = {
-			sounds : {},
-			sources : [{
-				lake : 'sounds/Whales Spouting Out Water-SoundBible.com-966355632.mp3',
-				splash : 'sounds/Depth Charge 2-SoundBible.com-338644910.mp3',
-				splash2 : 'sounds/Water Splash-SoundBible.com-800223477.mp3',
-				underwater : 'sounds/156011__hdvideoguy__scubadiving-01.mp3'
+			sounds: {},
+			sources: [{
+				lake: 'sounds/Whales Spouting Out Water-SoundBible.com-966355632.mp3',
+				splash: 'sounds/Depth Charge 2-SoundBible.com-338644910.mp3',
+				splash2: 'sounds/Water Splash-SoundBible.com-800223477.mp3',
+				underwater: 'sounds/156011__hdvideoguy__scubadiving-01.mp3'
 			}],
-			load : function(callback) {
+			load: function (callback) {
 				this.totalFiles = Object.size(this.sources[0]);
-				
-				for(var key in this.sources[0]) {
-					var sound = new Howl({ src: [this.sources[0][key]] }), self = this;
-					sound.once('load', function(key) {
-						return function() {
+
+				for (var key in this.sources[0]) {
+					var sound = new Howl({
+							src: [this.sources[0][key]]
+						}),
+						self = this;
+					sound.once('load', function (key) {
+						return function () {
 							self.sounds[key] = this;
-							if( Object.size(self.sounds) === self.totalFiles ) {
-								if( typeof callback === 'function' ) {
+							if (Object.size(self.sounds) === self.totalFiles) {
+								if (typeof callback === 'function') {
 									callback();
 								}
 							}
@@ -164,36 +163,36 @@
 					}(key));
 				}
 			},
-			loop : function(name) {
+			loop: function (name) {
 				this.sounds[name].loop(true);
 			},
-			volume : function(name, val) {
+			volume: function (name, val) {
 				this.sounds[name].volume(val);
 			},
-			play : function(name, time) {
+			play: function (name, time) {
 				this.sounds[name].stop();
 				var self = this;
-				setTimeout(function() {
+				setTimeout(function () {
 					self.sounds[name].play();
 				}, time || 0);
 			},
-			stop : function(name) {
+			stop: function (name) {
 				this.sounds[name].stop();
 			},
-			fadeIn : function(name, time) {
+			fadeIn: function (name, time) {
 				var self = this;
-				setTimeout(function() {
-					self.sounds[name].fade(0,1,1500);
+				setTimeout(function () {
+					self.sounds[name].fade(0, 1, 1500);
 				}, time || 0);
 			},
-			fadeOut : function(name, time) {
+			fadeOut: function (name, time) {
 				var self = this;
-				setTimeout(function() {
-					self.sounds[name].fade(1,0,1500);
+				setTimeout(function () {
+					self.sounds[name].fade(1, 0, 1500);
 				}, time || 0);
 			},
-			toggleMuteAll : function(state) {
-				for(var key in this.sounds) {
+			toggleMuteAll: function (state) {
+				for (var key in this.sounds) {
 					this.sounds[key].mute(state);
 				}
 			}
@@ -203,35 +202,31 @@
 		mainContainer.classList.add('landing-layout--loaded');
 
 		bubbles.init();
-		// alert(mainContainer.classList)
-		
-		
-		triggerCtrl.addEventListener('click', function() {
+		triggerCtrl.addEventListener('click', function () {
 			toggleMenu();
 		});
 
 	}
 
-	
+
 
 	function toggleMenu() {
-		
+
 		var isOpen = mainContainer.classList.contains('landing-layout--open');
 		// Toggle class on the main container.
 		mainContainer.classList.toggle('landing-layout--open');
 
-		if( isOpen ) { // close it.
+		if (isOpen) { // close it.
 			// Stop the bubbles canvas animation.
 			logo.classList.remove("hide");
-			
+
 			bubbles.stop();
 			// Sounds..
 			audio.fadeOut('underwater');
 			audio.play('splash2', 700);
 			audio.loop('lake');
 			audio.fadeIn('lake', 700);
-		}
-		else {	// open it.
+		} else { // open it.
 			// Start the bubbles canvas animation.
 			logo.classList.add("hide");
 			bubbles.start();
